0dc449bf828a59562105b1caf396f69d029d65b6
Archana Singh <archana.sofy@gmail.com>
2022-06-01 20:09:52 +0530

    refactor

--- src/sg_policy/test/test_map_in_list_ref.py
+++ src/sg_policy/test/test_map_in_list_ref.py
@@ -15,35 +15,25 @@ def apigw_event():
     """Generates API GW Event"""
 
     return {
-        "evaluator_data":
-               {
-                  "name":"fail"
-               }
-            ,
-            "input_data":[
-               {
-                  "rule_no":{
-                     "key":{
-                        "value":{
-                           "hello":11
-                        }
-                     }
-                  },
-                  "action":"allow",
-                  "from_port":"0",
-                  "to_port":0,
-                  "protocol":"-1",
-                  "cidr_block":"0.0.0.0/0"
-               }
-            ]
+        "evaluator_data": [{"name": "fail"}],
+        "input_data": [
+            {
+                "rule_no": {"key": {"value": {"hello": 11}}},
+                "action": "allow",
+                "from_port": "0",
+                "to_port": 0,
+                "protocol": "-1",
+                "cidr_block": "0.0.0.0/0",
             }
+        ],
+    }
+
 
 def test_evaluationresult_handler(apigw_event):
-    
-    from sg_policy.providers.python.terraform_plan import handler  
-    input_data=apigw_event["input_data"]
-    evaluator_data=apigw_event["evaluator_data"] 
-    result,iter_count = handler.map_in_list(input_data,evaluator_data)
-    
 
-    assert   result["pass"] in ["true"]
+    from sg_policy.providers.python.terraform_plan import handler
+
+    input_data = apigw_event["input_data"]
+    evaluator_data = apigw_event["evaluator_data"]
+    result, iter_count = handler.map_in_list(input_data, evaluator_data)
+    assert result["pass"] in ["true"]
