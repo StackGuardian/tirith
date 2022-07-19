be6eea3feeb4a85a8ac7679cb5d6e32d07b6466b
Arunim Chaudhary <arunim2@gmail.com>
2022-07-19 20:50:50 +0530

    update equals for unsorted lists

--- src/sg_policy/core/evaluators/Equals.py
+++ src/sg_policy/core/evaluators/Equals.py
@@ -1,46 +1,68 @@
 import BaseEvaluator
 
+# Checks if :attr:`value` is equal to :attr:`other`. Automatically casts values to the same type if possible.
+
+# Args:
+#     value (mixed): Value to compare.
+#     other (mixed): Other value to compare.
+
+# Returns:
+#     bool: Whether :attr:`value` is equal to :attr:`other`.
+
+# Example:
+
+#     >>> eq(None, None)
+#     True
+#     >>> eq(None, '')
+#     False
+#     >>> eq('a', 'a')
+#     True
+#     >>> eq(1, str(1))
+#     False
+
+# .. versionadded:: 1.0.0
+
 
 class Equals(BaseEvaluator):
-    def evaluate(value, other) -> bool:
-        # Checks if :attr:`value` is equal to :attr:`other`. Automatically casts values to the same type if possible.
+    def sort_lists_in_dicts(self, input):
+        for key in input:
+            if isinstance(input[key], list):
+                if isinstance(input[key][0], dict):
+                    sorted_array = []
+                    for index, item in enumerate(input[key]):
+                        sorted_array.append(self.sort_lists_in_dicts(input[key][index]))
+                    input[key] = sorted_array
+                else:
+                    input[key] = sorted(input[key])
+            if isinstance(input[key], dict):
+                self.sort_lists_in_dicts(input[key])
+        return input
 
-        # Args:
-        #     value (mixed): Value to compare.
-        #     other (mixed): Other value to compare.
+    def evaluate(self, evaluator_input, evaluator_data) -> bool:
 
-        # Returns:
-        #     bool: Whether :attr:`value` is equal to :attr:`other`.
-
-        # Example:
-
-        #     >>> eq(None, None)
-        #     True
-        #     >>> eq(None, '')
-        #     False
-        #     >>> eq('a', 'a')
-        #     True
-        #     >>> eq(1, str(1))
-        #     False
-
-        # .. versionadded:: 1.0.0
-        value1 = value
-        value2 = other
+        value1 = evaluator_input
+        value2 = evaluator_data
         if (
-            isinstance(value, str)
-            or isinstance(value, object)
-            or isinstance(value, list)
+            isinstance(evaluator_input, str)
+            or isinstance(evaluator_input, dict)
+            or isinstance(evaluator_input, list)
         ):
-            value1 = value
+            value1 = evaluator_input
         else:
-            value1 = str(value)
+            value1 = str(evaluator_data)
         if (
-            isinstance(other, str)
-            or isinstance(other, object)
-            or isinstance(other, list)
+            isinstance(evaluator_data, str)
+            or isinstance(evaluator_data, dict)
+            or isinstance(evaluator_data, list)
         ):
-            value2 = other
+            value2 = evaluator_data
         else:
-            value2 = str(other)
+            value2 = str(evaluator_data)
+
+        # sort all lists in dicts
+        if isinstance(value1, dict):
+            value1 = self.sort_lists_in_dicts(value1)
+        if isinstance(value2, dict):
+            value2 = self.sort_lists_in_dicts(value2)
 
         return value1 == value2
