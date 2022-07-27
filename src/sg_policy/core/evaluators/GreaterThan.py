c08c34f27d00567ae483091764508372fb61ee54
naveen8801 <naveensharma10d@gmail.com>
2022-07-27 20:54:57 +0530

    avoiding typecasting during comparision inside evaluators

--- src/sg_policy/core/evaluators/GreaterThan.py
+++ src/sg_policy/core/evaluators/GreaterThan.py
@@ -1,5 +1,6 @@
 import BaseEvaluator
 
+
 # Checks if :attr:`value` is more then :attr:`other`. Automatically casts values to the same type if possible.
 
 # Args:
@@ -23,23 +24,9 @@ import BaseEvaluator
 
 class GreaterThan(BaseEvaluator):
     def evaluate(self, evaluator_input, evaluator_data) -> bool:
-
-        value1 = evaluator_input
-        value2 = evaluator_data
-
-        # Currently supporting int, float and string
-        if (
-            isinstance(evaluator_input, str)
-            or isinstance(evaluator_input, int)
-            or isinstance(evaluator_input, float)
-        ) and (
-            isinstance(evaluator_data, str)
-            or isinstance(evaluator_data, int)
-            or isinstance(evaluator_data, float)
-        ):
-            value1 = str(evaluator_input)
-            value2 = str(evaluator_data)
-
+        try:
+            value1 = evaluator_input
+            value2 = evaluator_data
             return value1 > value2
-        else:
+        except:
             return False
