a4908338804b6b10e419ad47ba4e160e9cedd807
AksharSans <sanyalamitrakshar@gmail.com>
2022-07-28 19:45:00 +0530

    Updated test_Regex.py

--- src/sg_policy/core/evaluators/test_regex.py
+++
@@ -1,8 +0,0 @@
-import re
-import pytest
-
-evaluator_data = "^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$"
-evaluator_input = "123-456"
-def test_evaluator(evaluator_data, evaluator_input):
-    assert re.match(evaluator_data, evaluator_input) is not None
-
