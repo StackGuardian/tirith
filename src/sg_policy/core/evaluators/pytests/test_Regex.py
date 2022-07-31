2837b1b828b128c2dcec3b25b379dc1b29e61898
AksharSans <sanyalamitrakshar@gmail.com>
2022-07-31 18:02:02 +0530

    Added regex evaluator and pytest for regex evaluator

--- src/sg_policy/core/evaluators/pytests/test_Regex.py
+++ src/sg_policy/core/evaluators/pytests/test_Regex.py
@@ -1,11 +1,13 @@
 import re
 import pytest
 import sys
-sys.path.append('/home/aksharsans/Desktop/SG_policyframework/policy-framework/src/sg_policy/core/evaluators')
+sys.path.append('/policy-framework/src/sg_policy/core/evaluators')
 import Regex
 
 evaluator_data = "^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$"
 evaluator_input = "amitrakshar01"
+# evaluator_data = 45 #testing for the acceptance of data types other than string
+# evaluator_input = 2
 # def test_regexEquals(evaluator_data, evaluator_input):
 #     # assert re.match(evaluator_data, evaluator_input) is not None
 #     assert True
