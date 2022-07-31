036f06c4df4819d8518498620603b61765cc79e9
AksharSans <sanyalamitrakshar@gmail.com>
2022-07-31 18:02:02 +0530

    Added regex evaluator and pytest for regex evaluator

--- src/sg_policy/core/evaluators/Regex.py
+++ src/sg_policy/core/evaluators/Regex.py
@@ -3,8 +3,18 @@ import re
 
 class regexEquals(BaseEvaluator.BaseEvaluator):
     def evaluate(self, evaluator_input, evaluator_data) -> bool:
-        match = re.match(evaluator_data, evaluator_input)
-        if match is None:
-            return False
-        else:
-            return True
+        match = 0
+        if (type(evaluator_input) == str and type(evaluator_data) == str):
+            match = re.match(evaluator_data, evaluator_input)
+            if match is None:
+                return False
+            else:
+                return True
+        return False
+        
+        # match = 0
+        # match = re.match(evaluator_data, evaluator_input)
+        # if match is None:
+        #     return False
+        # else:
+        #     return True
