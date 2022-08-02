c4a9d498dc96fc447aab2f2041c2058b2d3d630c
naveen8801 <naveensharma10d@gmail.com>
2022-08-02 20:18:28 +0530

    try catch used

--- src/sg_policy/providers/python/infracost/handler.py
+++ src/sg_policy/providers/python/infracost/handler.py
@@ -1,31 +1,45 @@
 def __get_all_costs(costType, input_data):
     totalSum = 0
-    projects = input_data["projects"]
-    for project in projects:
-        resources = project['breakdown']['resources']
-        for resource in resources:
-            if resource[costType] != "null":
-                totalSum += float(resource[costType])
-    return totalSum
+    projects = input_data.get("projects")
+    if projects is not None:
+        for project in projects:
+            breakdown = project.get('breakdown')
+            if breakdown is not None:
+                resources = breakdown.get('resources')
+                if resources is not None:
+                    for resource in resources:
+                        if resource[costType] != "null":
+                            totalSum += float(resource[costType])
+                    return totalSum
 
 
 def __get_resources_costs(resource_type, costType, input_data):
     totalSum = 0
-    projects = input_data["projects"]
-    for project in projects:
-        resources = project['breakdown']['resources']
-        for resource in resources:
-            if resource[costType] != "null" and resource[name] in resource_type:
-                totalSum += float(resource[costType])
-    return totalSum
+    projects = input_data.get("projects")
+    if projects is not None:
+        for project in projects:
+            breakdown = project.get('breakdown')
+            if breakdown is not None:
+                resources = breakdown.get('resources')
+                if resources is not None:
+                    for resource in resources:
+                        if resource[costType] != "null" and resource[name] in resource_type:
+                            totalSum += float(resource[costType])
+                    return totalSum
 
 
 def provide(provider_inputs, input_data):
-    resource_type = provider_inputs['resource_type']
-    costType = provider_inputs['costType']
-    if not resource_type or resource_type == '*' or resource_type == ["*"]:
-        value = getValuesFromAll(costType, input_data)
-        return [{'value': value}]
-    else:
-        value = getValuesFromSet(resource_type, costType, input_data)
-        return [{'value': value}]
+    try:
+        resource_type = provider_inputs.get('resource_type')
+        costType = provider_inputs.get('costType')
+        if resource_type is not None and costType is not None:
+            if not resource_type or resource_type == '*' or resource_type == ["*"]:
+                value = getValuesFromAll(costType, input_data)
+                return [{'value': value}]
+            else:
+                value = getValuesFromSet(resource_type, costType, input_data)
+                return [{'value': value}]
+    except Exception as e:
+        print(e)
+        return None
+
