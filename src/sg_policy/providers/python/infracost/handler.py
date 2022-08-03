931819efe6237701fc47beced4abdbf70160e0ee
naveen8801 <naveensharma10d@gmail.com>
2022-08-03 08:58:10 +0530

    exception raised in child function and caught in parent

--- src/sg_policy/providers/python/infracost/handler.py
+++ src/sg_policy/providers/python/infracost/handler.py
@@ -1,45 +1,47 @@
 def __get_all_costs(costType, input_data):
     totalSum = 0
-    projects = input_data.get("projects")
-    if projects is not None:
-        for project in projects:
-            breakdown = project.get('breakdown')
-            if breakdown is not None:
-                resources = breakdown.get('resources')
-                if resources is not None:
-                    for resource in resources:
-                        if resource[costType] != "null":
-                            totalSum += float(resource[costType])
-                    return totalSum
+    if 'projects' in input_data:
+        for project in input_data.get("projects"):
+            if 'breakdown' in project and 'resources' in project.get('breakdown'):
+                for resource in project.get('breakdown').get('resources'):
+                    if resource[costType] != "null":
+                        totalSum += float(resource[costType])
+                return totalSum
+            else:
+                raise KeyError('key not found in one of the project')
+    else:
+        raise KeyError('projects key not found in input_data')
 
 
 def __get_resources_costs(resource_type, costType, input_data):
     totalSum = 0
-    projects = input_data.get("projects")
-    if projects is not None:
-        for project in projects:
-            breakdown = project.get('breakdown')
-            if breakdown is not None:
-                resources = breakdown.get('resources')
-                if resources is not None:
-                    for resource in resources:
-                        if resource[costType] != "null" and resource[name] in resource_type:
-                            totalSum += float(resource[costType])
-                    return totalSum
+    if 'projects' in input_data:
+        for project in input_data.get("projects"):
+            if 'breakdown' in project and 'resources' in project.get('breakdown'):
+                for resource in project.get('breakdown').get('resources'):
+                    if resource[costType] != "null" and resource['name'] in resource_type:
+                        totalSum += float(resource[costType])
+                return totalSum
+            else:
+                raise KeyError('key not found in one of the project')
+    else:
+        raise KeyError('projects key not found in input_data')
 
 
 def provide(provider_inputs, input_data):
     try:
-        resource_type = provider_inputs.get('resource_type')
-        costType = provider_inputs.get('costType')
-        if resource_type is not None and costType is not None:
+        if 'resource_type' in provider_inputs and 'costType' in provider_inputs:
+            resource_type = provider_inputs.get('resource_type')
+            costType = provider_inputs.get('costType')
             if not resource_type or resource_type == '*' or resource_type == ["*"]:
-                value = getValuesFromAll(costType, input_data)
+                value = __get_all_costs(costType, input_data)
                 return [{'value': value}]
             else:
-                value = getValuesFromSet(resource_type, costType, input_data)
+                value = __get_resources_costs(resource_type, costType, input_data)
                 return [{'value': value}]
-    except Exception as e:
+        else:
+            raise KeyError('key not found in provider_inputs')
+    except KeyError as e:
         print(e)
         return None
 
