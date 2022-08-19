ea2db66193c389b3a25cfcc507139806c6cfe68a
AksharSans <sanyalamitrakshar@gmail.com>
2022-08-19 21:03:25 +0530

    Removed commented code creating SonarCloud codesmells

--- src/sg_policy/core/evaluators/pytests/test_regex.py
+++ src/sg_policy/core/evaluators/pytests/test_regex.py
@@ -9,9 +9,6 @@ evaluator_input1 = "amitrakshar01"
 
 evaluator_data2 = "^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$"
 evaluator_input2 = "@01amitrakshar"
-# def test_regexEquals(evaluator_data, evaluator_input):
-#     # assert re.match(evaluator_data, evaluator_input) is not None
-#     assert True
 
 a = regex.RegexEquals()
 
@@ -20,7 +17,6 @@ a = regex.RegexEquals()
 def test_regex_passing():
     result = a.evaluate(evaluator_input1, evaluator_data1)
     assert result == {"result": True, "message": ""}
-    # assert a.evaluate(evaluator_input, evaluator_data) is False
     
 #pytest -v -m failing
 @pytest.mark.failing
