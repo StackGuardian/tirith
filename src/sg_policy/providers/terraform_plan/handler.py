3962aed1a6d04eece4510b7f8479e87ea2920420
Arunim Chaudhary <arunim2@gmail.com>
2022-08-22 19:37:03 +0530

    update tf plan provider

--- src/sg_policy/providers/terraform_plan/handler.py
+++ src/sg_policy/providers/terraform_plan/handler.py
@@ -36,7 +36,11 @@ from .utils import star_int_equals_int
 #   else:
 
 #     return "undef"
-def func(splitted_attr_name_expr, input_data):
+
+
+# input->(list [s3,acl,*],value of resource)
+# returns->[any, any, any]
+def get_expression_attribute(splitted_attr_name_expr, input_data):
 
     if len(splitted_attr_name_expr) == 0:
 
@@ -63,7 +67,7 @@ def func(splitted_attr_name_expr, input_data):
 
         # print("input_new    -",input_data_new)
 
-        func(splitted_attr_name_expr[1:], input_data_new)
+        get_expression_attribute(splitted_attr_name_expr[1:], input_data_new)
 
     elif lookup_key == "*" and isinstance(input_data, list):
         print("lookup key    -", lookup_key)
@@ -81,7 +85,7 @@ def func(splitted_attr_name_expr, input_data):
         input_data_new = values
         # print("1st data",splitted_attr_name_expr[1:])
         # print("2nd data",input_data_new)
-        func(splitted_attr_name_expr[1:], input_data_new)
+        get_expression_attribute(splitted_attr_name_expr[1:], input_data_new)
     elif lookup_key == "*" and isinstance(input_data, dict):
         values = []
         lookup_key = splitted_attr_name_expr[1:2][0]
@@ -94,11 +98,11 @@ def func(splitted_attr_name_expr, input_data):
         input_data_new = values
         # print("1st data",splitted_attr_name_expr[1:])
         # print("2nd data",input_data_new)
-        func(splitted_attr_name_expr[1:], input_data_new)
+        get_expression_attribute(splitted_attr_name_expr[1:], input_data_new)
 
     else:
-
-        return "undef"
+        # return blank array
+        return []
 
 
 def find_item(obj, key):
@@ -190,7 +194,82 @@ def get_attribute_name(input_resource_change_attrs, chunks):
     return input_changes
 
 
-def provide(policy, input_data):
+def provide(provider_inputs, input_data):
+    # """Provides the value of the attribute from the input_data"""
+    outputs = []
+    input_resource_change_attrs = {}
+    input_type = provider_inputs["input_type"]
+    resource_changes = input_data["resource_changes"]
+    # CASE 1
+    # - Get value of an attribute for all instances of a resource
+    # - resource_changes.*.change.after.<attr_name>
+    if input_type == "resource_changes":
+        attribute = provider_inputs["attribute"]
+        resource_type = provider_inputs["resource_type"]
+
+        for resource_change in resource_changes:
+            if resource_change["type"] == resource_type:
+                input_resource_change_attrs = resource_change["change"]["after"]
+                if attribute in input_resource_change_attrs:
+                    # attribute key found in the changes
+                    outputs.append(
+                        {
+                            "value": input_resource_change_attrs[attribute],
+                            "meta": resource_change,
+                            "err": None,
+                        }
+                    )
+                elif "." in attribute or "*" in attribute:
+                    evaluated_output = get_expression_attribute(
+                        attribute, input_resource_change_attrs
+                    )
+                    for val in evaluated_output:
+                        outputs.append(
+                            {"value": val, "meta": resource_change, "err": None}
+                        )
+
+        return outputs
+    # CASE 2
+    # - Get actions performed on a resource
+    # - resource_changes.*.change.actions
+    elif input_type == "resource_changes_actions":
+        resource_type = provider_inputs["resource_type"]
+        for resource_change in resource_changes:
+            if resource_change["type"] == resource_type:
+                outputs.append(
+                    {
+                        "value": resource_change["change"]["actions"],
+                        "meta": resource_change,
+                        "err": None,
+                    }
+                )
+        return outputs
+    # CASE 3
+    # - Get count of a particular resource
+    # - resource_changes.*.index
+    elif input_type == "resource_changes_count":
+        count = 0
+        resource_meta = {}
+        resource_type = provider_inputs["resource_type"]
+        for resource_change in resource_changes:
+            if resource_change["type"] == resource_type:
+                resource_meta = resource_change
+                count = min(count, resource_change["index"])
+
+        outputs.append(
+            {
+                "value": count,
+                "meta": resource_meta,
+                "err": None,
+            }
+        )
+        return outputs
+    # CASE 4
+    # TODO: Get relationship between resources
+    return outputs
+
+
+def provide_old(policy, input_data):
     """this function arranges the input changes per resource included in policy"""
     policies = policy["terraform_plan"]["policies"]
     policy_resource = []
@@ -224,7 +303,7 @@ def provide(policy, input_data):
                 policy_attribute_name.append(attribute["name"])
                 input_changes = get_attribute_name(input_resource_change_attrs, chunks)
 
-                msg_new = func(chunks, input_resource_change_attrs)
+                msg_new = get_expression_attribute(chunks, input_resource_change_attrs)
                 print(msg_new)
     # print(iter_count)
 
