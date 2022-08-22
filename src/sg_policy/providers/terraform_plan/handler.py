95766ac66023a74124140577a37db9f3cdc92423
Akshat Tandon <tandonakshat7@gmail.com>
2022-08-22 17:29:42 +0200

    private function

--- src/sg_policy/providers/terraform_plan/handler.py
+++ src/sg_policy/providers/terraform_plan/handler.py
@@ -4,7 +4,7 @@ import os
 import json
 from .utils import star_int_equals_int
 
-# TODO: use CAMELCASE for all function name
+## TODO: use CAMELCASE for all function name
 # def func(splitted_attr_name_expr,input_data):
 
 #   if len(splitted_attr_name_expr) == 0:
@@ -40,7 +40,7 @@ from .utils import star_int_equals_int
 
 # input->(list [s3,acl,*],value of resource)
 # returns->[any, any, any]
-def get_expression_attribute(splitted_attr_name_expr, input_data):
+def __get_expression_attribute(splitted_attr_name_expr, input_data):
 
     if len(splitted_attr_name_expr) == 0:
 
@@ -67,7 +67,7 @@ def get_expression_attribute(splitted_attr_name_expr, input_data):
 
         # print("input_new    -",input_data_new)
 
-        get_expression_attribute(splitted_attr_name_expr[1:], input_data_new)
+        __get_expression_attribute(splitted_attr_name_expr[1:], input_data_new)
 
     elif lookup_key == "*" and isinstance(input_data, list):
         print("lookup key    -", lookup_key)
@@ -85,7 +85,7 @@ def get_expression_attribute(splitted_attr_name_expr, input_data):
         input_data_new = values
         # print("1st data",splitted_attr_name_expr[1:])
         # print("2nd data",input_data_new)
-        get_expression_attribute(splitted_attr_name_expr[1:], input_data_new)
+        __get_expression_attribute(splitted_attr_name_expr[1:], input_data_new)
     elif lookup_key == "*" and isinstance(input_data, dict):
         values = []
         lookup_key = splitted_attr_name_expr[1:2][0]
@@ -98,101 +98,11 @@ def get_expression_attribute(splitted_attr_name_expr, input_data):
         input_data_new = values
         # print("1st data",splitted_attr_name_expr[1:])
         # print("2nd data",input_data_new)
-        get_expression_attribute(splitted_attr_name_expr[1:], input_data_new)
+        __get_expression_attribute(splitted_attr_name_expr[1:], input_data_new)
 
     else:
         # return blank array
         return []
-
-
-def find_item(obj, key):
-
-    if key in obj:
-        return obj[key]
-    for v in obj.values():
-        if isinstance(v, dict):
-            item = find_item(v, key)
-            if item is not None:
-                return item
-    return None
-
-
-def find_input_resource_changes_value(chunks, input_resource_change_attrs):
-    """Gives the filtered result after processing "*" and "." in query"""
-    Iter_Count = 0
-    if (
-        len(chunks) > 1
-        and len(input_resource_change_attrs[chunks[0]]) > 1
-        and (type(input_resource_change_attrs[chunks[0]]) == dict)
-    ):
-
-        res = find_item(input_resource_change_attrs[chunks[0]], chunks[-1])
-
-    elif (
-        len(chunks) > 1
-        and len(input_resource_change_attrs[chunks[0]]) == 1
-        and type(input_resource_change_attrs[chunks[0]]) == list
-    ):
-
-        res = find_item(input_resource_change_attrs[chunks[0]][0], chunks[-1])
-
-    elif (
-        len(chunks) > 1
-        and len(input_resource_change_attrs[chunks[0]]) > 1
-        and type(input_resource_change_attrs[chunks[0]]) == list
-    ):
-
-        res = []
-
-        for i in range(len(input_resource_change_attrs[chunks[0]])):
-            each_res = find_item(input_resource_change_attrs[chunks[0]][i], chunks[-1])
-            res.append(each_res)
-
-    else:
-        res = input_resource_change_attrs[chunks[0]]
-
-    return res
-
-
-# def find_item(chunks,input_resource_change_attrs):
-# 	print(chunks[-1])
-# 	print(input_resource_change_attrs[chunks[0]][0])
-# 	s=finditem(input_resource_change_attrs[chunks[0]][0],chunks[-1])
-# 	print(s)
-
-
-def get_attribute_name(input_resource_change_attrs, chunks):
-
-    if "*" in chunks:
-
-        if type(input_resource_change_attrs[chunks[0]]) == list:
-
-            res = find_input_resource_changes_value(chunks, input_resource_change_attrs)
-            # print(res)
-        else:
-
-            res = "undef"
-    elif len(chunks) > 1:
-        if type(input_resource_change_attrs[chunks[0]]) == list:
-
-            res = find_input_resource_changes_value(chunks, input_resource_change_attrs)
-            res = res[0]
-        else:
-
-            res = "undef"
-
-    else:
-        res = find_input_resource_changes_value(chunks, input_resource_change_attrs)
-        if len(chunks) > 1:
-            res = res[0]
-
-    try:
-        input_changes = int(res)
-
-    except:
-        input_changes = res
-    return input_changes
-
 
 def provide(provider_inputs, input_data):
     # """Provides the value of the attribute from the input_data"""
@@ -220,7 +130,7 @@ def provide(provider_inputs, input_data):
                         }
                     )
                 elif "." in attribute or "*" in attribute:
-                    evaluated_output = get_expression_attribute(
+                    evaluated_output = __get_expression_attribute(
                         attribute, input_resource_change_attrs
                     )
                     for val in evaluated_output:
@@ -267,53 +177,3 @@ def provide(provider_inputs, input_data):
     # CASE 4
     # TODO: Get relationship between resources
     return outputs
-
-
-def provide_old(policy, input_data):
-    """this function arranges the input changes per resource included in policy"""
-    policies = policy["terraform_plan"]["policies"]
-    policy_resource = []
-    policy_attribute_name = []
-    policy_attribute = []
-    input_resource_changes_attr_value = []
-    input_datatype = []
-    policy_attribute_name = []
-    input_resource_change_attrs = {}
-    new_policy_attribute = []
-    new_policy_attribute_name = []
-    count = 0
-    iter_count = 0
-    Iter_count = []
-    if len(policies) > 0:
-        for policy in policies:
-            policy_resource.append(policy["resource_type"])
-            attributes = policy["attributes"]
-        resource_changes = input_data["resource_changes"]
-        for resource_change in resource_changes:
-            if resource_change["type"] in policy_resource:
-                input_resource_change_attrs = resource_change["change"]["after"]
-        # print(input_resource_change_attrs)
-        for attribute in attributes:
-            count = 1
-            chunks = attribute["name"].split(".")
-            s = chunks[0]
-            if chunks[0] in input_resource_change_attrs:
-                count += 1
-                policy_attribute.append(attribute)
-                policy_attribute_name.append(attribute["name"])
-                input_changes = get_attribute_name(input_resource_change_attrs, chunks)
-
-                msg_new = get_expression_attribute(chunks, input_resource_change_attrs)
-                print(msg_new)
-    # print(iter_count)
-
-    msg = {
-        "policy_resource": policy_resource,
-        "policy_attribute": policy_attribute,
-        "policy_attribute_name": policy_attribute_name,
-        "input_resource_changes_attr_value": input_resource_changes_attr_value,
-        "input_datatype": input_datatype,
-        "iter_count": Iter_count,
-    }
-    # print(input_resource_changes_attr_value)
-    return msg
