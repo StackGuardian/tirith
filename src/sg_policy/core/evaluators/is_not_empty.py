022b41b3e6dc6408d2a7593da19db8d1010daaac
Arunim Chaudhary <arunim2@gmail.com>
2022-09-13 08:53:44 +0530

    fmt

--- src/sg_policy/core/evaluators/is_not_empty.py
+++ src/sg_policy/core/evaluators/is_not_empty.py
@@ -24,7 +24,11 @@ class IsNotEmpty(BaseEvaluator):
     def evaluate(self, evaluator_input, evaluator_data=None):
         evaluation_result = {"passed": False, "message": "Not evaluated"}
         try:
-            if (isinstance(evaluator_input, str) or isinstance(evaluator_input, list) or isinstance(evaluator_input, dict)) and evaluator_input:
+            if (
+                isinstance(evaluator_input, str)
+                or isinstance(evaluator_input, list)
+                or isinstance(evaluator_input, dict)
+            ) and evaluator_input:
                 evaluation_result["passed"] = True
                 evaluation_result["message"] = "{} is not empty".format(evaluator_input)
             if not evaluation_result["passed"]:
