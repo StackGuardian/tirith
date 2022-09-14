d4806686e0b6036f353d6883173481d8c4c7e86b
Akshat Tandon <tandonakshat7@gmail.com>
2022-09-14 09:28:02 +0200

    fix formatting

--- tests/providers/sg_workflow/tests_sg_workflow.py
+++ tests/providers/sg_workflow/tests_sg_workflow.py
@@ -13,180 +13,211 @@ def load_sg_workflow_plan_json(json_path):
 input_data = load_sg_workflow_plan_json("input.json")
 
 
-
 provider_args_1 = {
     "resource_type": "ResourceType",
 }
 
+
 def test_case_passing_1():
-    res = handler.provide(provider_args_1,input_data)
+    res = handler.provide(provider_args_1, input_data)
     assert bool(res[0]["value"] == "WORKFLOW") == True
 
 
 def test_case_failing_1():
-    res = handler.provide(provider_args_1,input_data)
+    res = handler.provide(provider_args_1, input_data)
     assert bool(res[0]["value"] != "WORKFLOW") == False
 
-#----------------------------------------------------------------------------- 
+
+# -----------------------------------------------------------------------------
 
 provider_args_2 = {
     "resource_type": "WfType",
 }
 
+
 def test_case_passing_2():
-    res = handler.provide(provider_args_2,input_data)
+    res = handler.provide(provider_args_2, input_data)
     assert bool(res[0]["value"] == "TERRAFORM") == True
 
 
 def test_case_failing_2():
-    res = handler.provide(provider_args_2,input_data)
+    res = handler.provide(provider_args_2, input_data)
     assert bool(res[0]["value"] != "TERRAFORM") == False
 
-#----------------------------------------------------------------------------- 
+
+# -----------------------------------------------------------------------------
 
 provider_args_3 = {
     "resource_type": "approvalPreApply",
 }
 
+
 def test_case_passing_3():
-    res = handler.provide(provider_args_3,input_data)
+    res = handler.provide(provider_args_3, input_data)
     assert bool(res[0]["value"] == True) == True
 
 
 def test_case_failing_3():
-    res = handler.provide(provider_args_3,input_data)
+    res = handler.provide(provider_args_3, input_data)
     assert bool(res[0]["value"] != True) == True
 
 
-#----------------------------------------------------------------------------- 
+# -----------------------------------------------------------------------------
 provider_args_4 = {
     "resource_type": "driftCheck",
 }
+
+
 def test_case_passing_4():
-    res = handler.provide(provider_args_4,input_data)
+    res = handler.provide(provider_args_4, input_data)
     assert bool(res[0]["value"] == False) == True
 
 
 def test_case_failing_4():
-    res = handler.provide(provider_args_4,input_data)
+    res = handler.provide(provider_args_4, input_data)
     assert bool(res[0]["value"] != False) == False
-#----------------------------------------------------------------------------- 
+
+
+# -----------------------------------------------------------------------------
 provider_args_5 = {
     "resource_type": "managedTerraformState",
 }
+
+
 def test_case_passing_5():
-    res = handler.provide(provider_args_5,input_data)
+    res = handler.provide(provider_args_5, input_data)
     assert bool(res[0]["value"] == True) == True
 
+
 def test_case_failing_5():
-    res = handler.provide(provider_args_5,input_data)
+    res = handler.provide(provider_args_5, input_data)
     assert bool(res[0]["value"] != True) == False
 
-#----------------------------------------------------------------------------- 
+
+# -----------------------------------------------------------------------------
 # Problem in handler
 provider_args_8 = {
     "resource_type": "s3_bucket_acl",
 }
 
+
 def test_case_passing_8():
-    res = handler.provide(provider_args_8,input_data)
+    res = handler.provide(provider_args_8, input_data)
     assert bool(res[0]["value"] == "public-read") == True
 
 
 def test_case_failing_8():
-    res = handler.provide(provider_args_8,input_data)
+    res = handler.provide(provider_args_8, input_data)
     assert bool(res[0]["value"] != "public-read") == False
 
-#----------------------------------------------------------------------------- 
+
+# -----------------------------------------------------------------------------
 # Problem in handler
 provider_args_9 = {
     "resource_type": "s3_bucket_block_public_acls",
 }
 
+
 def test_case_passing_9():
-    res = handler.provide(provider_args_9,input_data)
+    res = handler.provide(provider_args_9, input_data)
     assert bool(res[0]["value"] == False) == True
 
 
 def test_case_failing_9():
-    res = handler.provide(provider_args_9,input_data)
+    res = handler.provide(provider_args_9, input_data)
     assert bool(res[0]["value"] != False) == True
-#----------------------------------------------------------------------------- 
+
+
+# -----------------------------------------------------------------------------
 # Problem in handler
 provider_args_10 = {
     "resource_type": "s3_bucket_block_public_policy",
 }
 
+
 def test_case_passing_10():
-    res = handler.provide(provider_args_10,input_data)
+    res = handler.provide(provider_args_10, input_data)
     assert bool(res[0]["value"] == False) == True
 
 
 def test_case_failing_10():
-    res = handler.provide(provider_args_10,input_data)
+    res = handler.provide(provider_args_10, input_data)
     assert bool(res[0]["value"] != False) == True
-#----------------------------------------------------------------------------- 
+
+
+# -----------------------------------------------------------------------------
 # Problem in handler
 provider_args_11 = {
     "resource_type": "s3_bucket_force_destroy",
 }
 
+
 def test_case_passing_11():
-    res = handler.provide(provider_args_11,input_data)
+    res = handler.provide(provider_args_11, input_data)
     assert bool(res[0]["value"] == True) == True
 
+
 def test_case_failing_11():
-    res = handler.provide(provider_args_11,input_data)
+    res = handler.provide(provider_args_11, input_data)
     assert bool(res[0]["value"] != True) == True
 
-#----------------------------------------------------------------------------- 
+
+# -----------------------------------------------------------------------------
 # Problem in handler
 provider_args_12 = {
     "resource_type": "s3_bucket_ignore_public_acls",
 }
 
+
 def test_case_passing_12():
-    res = handler.provide(provider_args_12,input_data)
+    res = handler.provide(provider_args_12, input_data)
     assert bool(res[0]["value"] == False) == True
 
+
 def test_case_failing_12():
-    res = handler.provide(provider_args_12,input_data)
+    res = handler.provide(provider_args_12, input_data)
     assert bool(res[0]["value"] != False) == True
 
-#-----------------------------------------------------------------------------
-# # Problem in handler 
+
+# -----------------------------------------------------------------------------
+# # Problem in handler
 provider_args_13 = {
     "resource_type": "s3_bucket_restrict_public_buckets",
 }
 
+
 def test_case_passing_13():
-    res = handler.provide(provider_args_13,input_data)
+    res = handler.provide(provider_args_13, input_data)
     assert bool(res[0]["value"] == False) == True
 
 
 def test_case_failing_13():
-    res = handler.provide(provider_args_13,input_data)
+    res = handler.provide(provider_args_13, input_data)
     assert bool(res[0]["value"] != False) == True
-#----------------------------------------------------------------------------- 
+
+
+# -----------------------------------------------------------------------------
 provider_args_14 = {
     "resource_type": "useMarketplaceTemplate",
 }
 
+
 def test_case_passing_14():
-    res = handler.provide(provider_args_14,input_data)
+    res = handler.provide(provider_args_14, input_data)
     assert bool(res[0]["value"] == True) == True
 
 
 def test_case_failing_14():
-    res = handler.provide(provider_args_14,input_data)
+    res = handler.provide(provider_args_14, input_data)
     assert bool(res[0]["value"] != True) == False
 
-#----------------------------------------------------------------------------- 
+
+# -----------------------------------------------------------------------------
 # provider_args_15 = {
 #     "resource_type": "iacTemplateId",
 # }
 
-#----------------------------------------------------------------------------- 
+# -----------------------------------------------------------------------------
 # provider_args_6 = {
 #     "resource_type": "terraformVersion",
 # }
--- tests/providers/terraform_plan/test_terraform_plan.py
+++ tests/providers/terraform_plan/test_terraform_plan.py
@@ -40,17 +40,19 @@ provider_args_4 = {
     "terraform_resource_attribute": "actions",
 }
 
-# If the value of the attribute "enable_dns_hostnames" is given false, the test case should pass, otherwise fail case should pass 
+# If the value of the attribute "enable_dns_hostnames" is given false, the test case should pass, otherwise fail case should pass
 @pytest.mark.passing
 def test_awsvpc_attribute_status_pass():
     res = handler.provide(provider_args_1, input_data)
     assert res[0]["value"] == False
 
-# If the value of the attribute "enable_dns_support" is expected to be True, then the pass test test case should be considered or else the fail case should be considered  
+
+# If the value of the attribute "enable_dns_support" is expected to be True, then the pass test test case should be considered or else the fail case should be considered
 @pytest.mark.passing
 def test_awsvpc_attribute_status_pass():
     res = handler.provide(provider_args_2, input_data)
     assert res[0]["value"] == True
+
 
 @pytest.mark.failing
 def test_awsvpc_attribute_status_fail():
@@ -62,14 +64,13 @@ def test_awsvpc_attribute_status_fail():
 @pytest.mark.passing
 def test_awsvpc_actions_pass():
     res = handler.provide(provider_args_4, input_data)
-    assert bool(res[0]["value"] == 'create') == True
+    assert bool(res[0]["value"] == "create") == True
 
 
 @pytest.mark.failing
 def test_awsvpc_actions_fail():
     res = handler.provide(provider_args_4, input_data)
-    assert bool(res[0]["value"] != 'create') == False # output to be False
-
+    assert bool(res[0]["value"] != "create") == False  # output to be False
 
 
 # If the count of the resources matches with what user expects, then the pass test caset to be considered and if not then the fail test case to be considered
@@ -78,11 +79,12 @@ def test_get_attribute_name_passing7():
     res = handler.provide(provider_args_3, input_data)
     assert res[0]["value"] > 0
 
+
 @pytest.mark.failing
 def test_get_attribute_name_failing8():
     res = handler.provide(provider_args_3, input_data)
-    assert res[0]["value"] !=0
+    assert res[0]["value"] != 0
 
 
 # if count == 0, no services deployed
-# if count > 0 services deployed 
+# if count > 0 services deployed
