509426c5351816c4e96baedd08587dc798df79f2
Rafid Aslam (refeed) <rafidteam@gmail.com>
2022-10-12 17:37:22 +0700

    IsEmpty: Evaluate None as empty

--- tests/core/evaluators/test_is_empty.py
+++ tests/core/evaluators/test_is_empty.py
@@ -1,20 +1,21 @@
 from tirith.core.evaluators import IsEmpty
 from pytest import mark
 
-evaluator_input1 = ""
-evaluator_input2 = "stackguardian"
+checks_passing = ("", None, [], dict())
+checks_failing = ("stackguardian", 1, [None], dict(a=1))
 
 evaluator = IsEmpty()
 
-# pytest -v -m passing
+
 @mark.passing
-def test_evaluate_passing():
-    result = evaluator.evaluate(evaluator_input1)
-    assert result == {"message": f"{evaluator_input1} is empty", "passed": True}
+@mark.parametrize("evaluator_input", checks_passing)
+def test_evaluate_passing(evaluator_input):
+    result = evaluator.evaluate(evaluator_input)
+    assert result == {"message": f"{evaluator_input} is empty", "passed": True}
 
 
-# pytest -v -m passing
-@mark.failing
-def test_evaluate_failing():
-    result = evaluator.evaluate(evaluator_input2)
-    assert result == {"message": f"{evaluator_input2} is not empty", "passed": False}
+@mark.passing
+@mark.parametrize("evaluator_input", checks_failing)
+def test_evaluate_failing(evaluator_input):
+    result = evaluator.evaluate(evaluator_input)
+    assert result == {"message": f"{evaluator_input} is not empty", "passed": False}
