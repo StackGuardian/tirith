83d2d5ada54c5f8abea74c0534e104286157a2cb
Rafid Aslam (refeed) <rafidteam@gmail.com>
2022-10-16 18:55:23 +0700

    core: Remove undefined eval ids and None from the final eval exp
    Closes https://github.com/StackGuardian/tirith/issues/80

---
+++ tests/core/test_core.py
@@ -0,0 +1,40 @@
+# Maintain all core related tests here
+from pytest import mark
+
+from tirith.core.core import final_evaluator
+
+
+@mark.passing
+def test_final_evaluator_skipped_check_should_be_removed():
+    actual_result = final_evaluator("!skipped_check && passing_check", dict(skipped_check=None, passing_check=True))
+    assert actual_result == (True, [])
+
+    actual_result = final_evaluator("!skipped_check && passing_check", dict(skipped_check=None, passing_check=False))
+    assert actual_result == (False, [])
+
+
+@mark.passing
+def test_final_evaluator_undef_var_should_be_removed_from_exp():
+    actual_result = final_evaluator(
+        "!skipped_check && passing_check || undefined_check", dict(skipped_check=None, passing_check=True)
+    )
+    assert actual_result == (
+        True,
+        ["The following evaluator ids are not defined and have been removed: undefined_check"],
+    )
+
+    actual_result = final_evaluator(
+        "!skipped_check && passing_check || undefined_check", dict(skipped_check=None, passing_check=False)
+    )
+    assert actual_result == (
+        False,
+        ["The following evaluator ids are not defined and have been removed: undefined_check"],
+    )
+
+
+@mark.passing
+def test_final_evaluator_malicious_eval_should_err():
+    actual_result = final_evaluator(
+        "!skipped_check && passing_check || [].__class__.__base__", dict(skipped_check=None, passing_check=True)
+    )
+    assert actual_result == (False, ["The following symbols are not allowed: __class__, __base__"])
--- tests/core/tests.py
+++
@@ -1,1 +0,0 @@
-# Maintain all core related tests here
