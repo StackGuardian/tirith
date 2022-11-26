a650c12915b2d0ae6ab47ef67ff58d4e9f3335d3
Akshat Tandon <tandonakshat7@gmail.com>
2022-11-26 18:28:05 +0100

    py-tirith

--- src/tirith/__init__.py
+++ src/tirith/__init__.py
@@ -5,6 +5,3 @@ tirith: Execute policies defined using Tirith (StackGuardian Policy Framework)
 __version__ = "1.0.0-alpha.1"
 __author__ = "StackGuardian"
 __license__ = "Apache"
-
-
-from .core.core import start_policy_evaluation
