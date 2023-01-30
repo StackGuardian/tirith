b01ddd7718456e3cbddec1f6b17f79e7c1e4491a
Akshat Tandon <tandonakshat7@gmail.com>
2023-01-30 20:46:00 +0100

    version bump

--- src/tirith/__init__.py
+++ src/tirith/__init__.py
@@ -2,6 +2,6 @@
 tirith: Execute policies defined using Tirith (StackGuardian Policy Framework)
 """
 
-__version__ = "1.0.0-alpha.2"
+__version__ = "1.0.0-alpha.3"
 __author__ = "StackGuardian"
 __license__ = "Apache"
