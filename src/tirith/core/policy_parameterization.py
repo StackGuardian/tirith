ac871eab15ca90b3d9b85dcd3f3bb60da5bc3be9
Samriddhi Singh <samriddhisingh6079@gmail.com>
2024-08-28 14:29:02 +0530

    Use pydash.get() and check for match only if the value is of type str

--- src/tirith/core/policy_parameterization.py
+++ src/tirith/core/policy_parameterization.py
@@ -1,18 +1,10 @@
 import re
+import pydash
 
 
 def check_match(string: str, pattern: re.Pattern) -> re.Match:
     match_ = re.fullmatch(pattern, string)
     return match_
-
-
-# traversing var_dict (esp for nested cases)
-def get_var(data: dict, path: str):
-    keys = path.split(".")
-    value = data
-    for key in keys:
-        value = value[key]
-    return value
 
 
 def replace_vars(policy_dict: dict, var_dict: dict) -> dict:
@@ -22,13 +14,15 @@ def replace_vars(policy_dict: dict, var_dict: dict) -> dict:
 
     for i in range(len(evaluators)):
         for key, value in evaluators[i]["provider_args"].items():
-            match = check_match(value, var_pattern)
-            if bool(match):
-                evaluators[i]["provider_args"][key] = get_var(var_dict, match.group(1))
+            if isinstance(value, str):
+                match = check_match(value, var_pattern)
+                if bool(match):
+                    evaluators[i]["provider_args"][key] = pydash.get(var_dict, match.group(1))
 
         for key, value in evaluators[i]["condition"].items():
-            match = check_match(value, var_pattern)
-            if bool(match):
-                evaluators[i]["condition"][key] = get_var(var_dict, match.group(1))
+            if isinstance(value, str):
+                match = check_match(value, var_pattern)
+                if bool(match):
+                    evaluators[i]["condition"][key] = pydash.get(var_dict, match.group(1))
 
     return policy_dict
