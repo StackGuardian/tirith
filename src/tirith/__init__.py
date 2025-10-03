b57fefe212e3c60007000557caafb287ab74c62a
Rafid Aslam <rafid.aslam@stackguardian.io>
2025-10-03 16:20:56 +0700

    bump version (#265)

--- src/tirith/__init__.py
+++ src/tirith/__init__.py
@@ -2,6 +2,6 @@
 tirith: Execute policies defined using Tirith (StackGuardian Policy Framework)
 """
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __author__ = "StackGuardian"
 __license__ = "Apache"
